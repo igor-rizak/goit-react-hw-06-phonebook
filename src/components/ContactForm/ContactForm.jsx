26479ffdf4c74750d9e61502b81290dc564ee686
igor-rizak <108016908+igor-rizak@users.noreply.github.com>
2023-08-09 14:10:47 +0300

    Initial commit

---
+++ src/components/ContactForm/ContactForm.jsx
@@ -0,0 +1,68 @@
+import { useState} from 'react';
+import { nanoid } from 'nanoid';
+import { Form, Label, Button, Input } from './ContactForm.styled';
+
+const ContactForm = ({ onSubmit }) => {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
+
+  const nameInputId = nanoid();
+  const numberInputId = nanoid();
+
+  const handleSubmit = event => {
+    event.preventDefault();
+
+    onSubmit({ name, number });
+    setName('');
+    setNumber('');
+  };
+
+  const handleChange = e => {
+    const { name, value } = e.target;
+
+    switch (name) {
+      case 'name':
+        setName(value);
+        break;
+      case 'number':
+        setNumber(value);
+        break;
+      default:
+        return;
+    }
+  };
+
+  return (
+    <Form onSubmit={handleSubmit}>
+      <Label htmlFor={nameInputId}>
+        Name
+        <Input
+          type="text"
+          name="name"
+          onChange={handleChange}
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          required
+          value={name}
+        />
+      </Label>
+
+      <Label htmlFor={numberInputId}>
+        Number
+        <Input
+          type="tel"
+          name="number"
+          value={number}
+          onChange={handleChange}
+          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+          required
+        />
+      </Label>
+
+      <Button type="submit">Add contact</Button>
+    </Form>
+  );
+};
+
+export default ContactForm;
